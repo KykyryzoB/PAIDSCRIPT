b38a9258eec4a028429a2a8524d76288b96cfe0d
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2025-04-18 23:24:06 +0500

    Update abc.lua

--- abc.lua
+++ abc.lua
@@ -455,6 +455,19 @@ local Button = Tab:CreateButton({
     end,
 })
 
+local Section = Tab:CreateSection("FIND ALL BIG MY EGGS")
+
+local Button = Tab:CreateButton({
+    Name = "FIND",
+    Callback = function()
+        for i,v in ipairs(workspace.EasterHuntEggs:GetDescendants()) do
+            if v:IsA("ClickDetector") then
+                fireclickdetector(v)
+            end
+        end
+    end,
+})
+
 local Section = Tab:CreateSection("Maze Glove")
 
 local Button = Tab:CreateButton({
@@ -1794,6 +1807,67 @@ elseif game.PlaceId == 117232463555132 then
         Callback = function() 
             workspace.Map.Components.GloveIsland.ClaimGlove.ProximityPrompt.MaxActivationDistance = 1000
             fireproximityprompt(workspace.Map.Components.GloveIsland.ClaimGlove.ProximityPrompt)
+        end,
+    })
+
+elseif game.PlaceId == 129665246576996 then
+
+    local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+    local Window = Rayfield:CreateWindow({
+        Name = "EGGS HUB",
+        Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+        LoadingTitle = "Loading Hub",
+        LoadingSubtitle = "by Kykyryz0B",
+        Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+     
+        DisableRayfieldPrompts = false,
+        DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+     
+        ConfigurationSaving = {
+           Enabled = false,
+           FolderName = nil, -- Create a custom folder for your hub/game
+           FileName = "Big Hub"
+        },
+     
+        Discord = {
+           Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+           Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+           RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        },
+     
+        KeySystem = true, -- Set this to true to use our key system
+        KeySettings = {
+           Title = "Key",
+           Subtitle = "Key System",
+           Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+           FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+           SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+           GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+           Key = {"1488"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+        }
+    })
+
+    local Tab = Window:CreateTab("Combat", "rewind")
+
+    Tab:CreateButton({
+        Name = "GET 1 GOLD EGG",
+        Callback = function() 
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.TrialCompletedPoints["Trial 1"].root.CFrame
+        end,
+    })
+
+    Tab:CreateButton({
+        Name = "GET 2 GOLD EGG",
+        Callback = function() 
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.TrialCompletedPoints["Trial 2"].root.CFrame
+        end,
+    })
+
+    Tab:CreateButton({
+        Name = "GET 3 GOLD EGG",
+        Callback = function() 
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.TrialCompletedPoints["Trial 3"].root.CFrame
         end,
     })
 
